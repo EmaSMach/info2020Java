2273e7fdc72fcf736eb8079f8349c44cc5b4cf7f
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2020-11-11 13:48:30 -0300

    weeker (tells the name of the day accorging to the given number)

---
+++ guiadas/WeekerSwitch.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumno
+ */
+import java.util.Scanner;
+
+public class WeekerSwitch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int day = scan.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Fuera de rango");
+        }
+    }
+}
